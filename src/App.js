9b7c6bdfd019c978cd4ae61728f7578711de8102
sandeepsharma2002 <sandeepsharmajec24@gmail.com>
2023-05-20 13:53:12 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
     <div style={{background: "#f5f5f5"}}>
       
       <Routes>
-        <Route path='/' element={<Dashboard />}/>
+        <Route path='/DashBoard' element={<Dashboard />}/>
         <Route path='login' element={<LoginWindow/>}/>
       </Routes>
     </div>
